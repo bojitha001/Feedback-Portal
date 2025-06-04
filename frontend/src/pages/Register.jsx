64792d27c34f4a76b9ee8cc7efde3263ec58ec8d
bojitha001 <nawarathnabojitha@gmail.com>
2025-06-04 21:48:13 +0530

    Implemented the user Registration to frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,4 +1,66 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
-  return <div>TODO: Implement Register Page</div>;
-} 
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+
+  const addUser = async (userData) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/auth/signup",
+        userData
+      );
+
+      const { user, token } = response.data;
+
+      localStorage.setItem("token", token);
+      console.log(user);
+      navigate("/");
+    } catch (error) {
+      console.error(
+        "Feedback submission failed:",
+        error.response?.data || error.message
+      );
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    addUser({
+      email: formData.email,
+      password: formData.password,
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+          type="text"
+          required
+          onChange={(event) =>
+            setFormData({ ...formData, email: event.target.value })
+          }
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          required
+          onChange={(event) =>
+            setFormData({ ...formData, password: event.target.value })
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
