5c430379433909a1978a8307d7be41e8d71711f9
bojitha001 <nawarathnabojitha@gmail.com>
2025-06-05 03:12:18 +0530

    Implement the functionality to submit the feedback

--- frontend/src/pages/SubmitFeedback.jsx
+++ frontend/src/pages/SubmitFeedback.jsx
@@ -1,4 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
+import NavBar from "../components/NavBar";
+import axios from "axios";
 export default function SubmitFeedback() {
-  return <div>TODO: Implement Submit Feedback Page</div>;
-} 
+  const [formData, setFormData] = useState({
+    message: "",
+  });
+
+  const addFeeddBack = async (feedBacks) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.post(
+        "http://localhost:3001/feedback",
+        feedBacks,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const { feedback } = response.data;
+      console.log(feedback);
+    } catch (error) {
+      console.log("Error while submitting data");
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    addFeeddBack({
+      message: formData.message,
+    });
+  };
+
+  return (
+    <div>
+      <NavBar />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="">FeedBack</label>
+        <input
+          type="text"
+          onChange={(event) =>
+            setFormData({ ...formData, message: event.target.value })
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
