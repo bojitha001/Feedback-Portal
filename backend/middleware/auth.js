f9550568bafa3f4aca6e2853b430f9a9e3a0cf72
bojitha001 <nawarathnabojitha@gmail.com>
2025-06-04 20:25:42 +0530

    Implement middleware to verify the users

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,39 @@
-const jwt = require('jsonwebtoken');
+const { PrismaClient } = require("@prisma/client");
+const jwt = require("jsonwebtoken");
 
-module.exports = function (req, res, next) {
+const JWT_SECRET = process.env.JWT_SECRET;
+const prisma = new PrismaClient();
+
+module.exports = async function (req, res, next) {
   // TODO: Extract and verify JWT token
-  next();
+  try {
+    const authHeader = req.header("Authorization");
+
+    if (!authHeader) {
+      return res.status(401).json({
+        error: "Invalid token format.",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+
+    const checkingUser = await prisma.user.findUnique({
+      where: { id: decodedInfo.userId },
+      select: {
+        id: true,
+        email: true,
+        isAdmin: true,
+      },
+    });
+
+    if (!checkingUser) {
+      return res.status(401).json({
+        error: "User not found",
+      });
+    }
+    
+
+    next();
+  } catch (error) {}
 };
