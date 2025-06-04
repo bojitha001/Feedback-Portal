a22e9b0fed31d08a447fd30483c2f2efd48d1865
bojitha001 <nawarathnabojitha@gmail.com>
2025-06-04 21:48:32 +0530

    implemented the user login to the frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,64 @@
-import React from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
-  return <div>TODO: Implement Login Page</div>;
-} 
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  
+  const loginUser = async (userData) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/auth/login",
+        userData
+      );
+
+      const { user, token } = response.data;
+      localStorage.setItem("token", token);
+      console.log(user);
+      navigate("/")
+    } catch (error) {
+      console.error(
+        "Feedback submission failed:",
+        error.response?.data || error.message
+      );
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    loginUser({
+      email: formData.email,
+      password: formData.password,
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+          type="text"
+          required
+          onChange={(event) =>
+            setFormData({ ...formData, email: event.target.value })
+          }
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          required
+          onChange={(event) =>
+            setFormData({ ...formData, password: event.target.value })
+          }
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
