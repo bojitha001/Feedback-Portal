bb8fd95047fa091bd394662f0c4916098df1b7aa
bojitha001 <nawarathnabojitha@gmail.com>
2025-06-04 13:31:52 +0530

    Add user signup functionality to backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,21 +1,63 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { PrismaClient } = require('@prisma/client');
+const bcrypt = require("bcrypt");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+const createToken = (userId, email, isAdmin) => {
+  return jwt.sign(
+    {
+      userId,
+      email,
+      isAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "24h",
+    }
+  );
+};
+
 // POST /signup
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   // TODO: Handle signup (create user, hash password, return JWT)
-  res.status(501).json({ message: 'Not implemented. Implement signup logic.' });
+  const { email, password, isAdmin } = req.body;
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+      data: {
+        email: email.toLowerCase(),
+        password: hashedPassword,
+        isAdmin,
+      },
+      select: {
+        id: true,
+        email: true,
+        isAdmin: true,
+      },
+    });
+
+    const token = createToken(user.id, user.email, user.isAdmin);
+
+    res.status(201).json({
+      user,
+      token,
+    });
+    console.log(user, token)
+  } catch (error) {
+    console.log("Register error", error);
+    res.status(400).send("User not created");
+  }
 });
 
 // POST /login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   // TODO: Handle login (check user, compare password, return JWT)
-  res.status(501).json({ message: 'Not implemented. Implement login logic.' });
+  const { email, password } = req.body;
 });
 
 module.exports = router;
