807c8a02710aa69a3d91a2915dc535003f7ab5ad
bojitha001 <nawarathnabojitha@gmail.com>
2025-06-05 14:26:36 +0530

    Implement the functinality to get the users feedbacks in frontend

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,4 +1,36 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 export default function AdminDashboard() {
-  return <div>TODO: Implement Admin Dashboard Page</div>;
-} 
+  const [feedBacks, setFeedBacks] = useState([]);
+
+  useEffect(() => {
+    async function fetchFeedBacks() {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "http://localhost:3001/admin/feedback",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response.data)
+        setFeedBacks(response.data.feedbacks)
+      } catch (error) {
+        console.log(error, "Error while fetching feedbacks");
+      }
+    }
+    fetchFeedBacks()
+  }, []);
+  return <div>
+    <NavBar/>
+    {feedBacks.map((feedBack) => (
+      <div key={feedBack.id}>
+      <p>{feedBack.message}</p>
+      <p>{feedBack.user.email}</p>
+      </div>
+    ))}
+  </div>;
+}
