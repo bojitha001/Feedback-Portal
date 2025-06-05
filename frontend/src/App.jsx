396f2af9368f9a8064522247c46e374bb4ab7f4a
bojitha001 <nawarathnabojitha@gmail.com>
2025-06-05 18:53:05 +0530

    Make styles for the DashBoard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Register from './pages/Register';
 import SubmitFeedback from './pages/SubmitFeedback';
 import MyFeedback from './pages/MyFeedback';
 import AdminDashboard from './pages/AdminDashboard';
+import HomePage from './pages/HomePage';
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Landing Page</div>} />
+        <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/submit" element={<SubmitFeedback />} />
